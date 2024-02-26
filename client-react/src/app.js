0b892b4aa021d95f936ebd4e18d2336c9e1b0900
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-02-26 17:16:53 +0530

    separate page for chat

--- client-react/src/app.js
+++ client-react/src/app.js
@@ -1,4 +1,24 @@
+// import { useState } from "react";
+
+// import AuthPage from "./authPage";
+// import ChatsPage from "./chatsPage";
+
+// function App() {
+//   const [user, setUser] = useState();
+//   console.log("USER", user)
+
+//   if (!user) {
+//     return <AuthPage onAuth={(user) => setUser(user)} />;
+//   } else {
+//     return <ChatsPage user={user} />;
+//   }
+// }
+
+// export default App;
+
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 import AuthPage from "./authPage";
 import ChatsPage from "./chatsPage";
@@ -7,11 +27,14 @@ function App() {
   const [user, setUser] = useState();
   console.log("USER", user)
 
-  if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
-  } else {
-    return <ChatsPage user={user} />;
-  }
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={!user ? <AuthPage onAuth={(user) => setUser(user)} /> : <Navigate to="/chats" />} />
+        <Route path="/chats" element={user ? <ChatsPage user={user} /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
